d195691fe58922abd6b81fd78e6ba2d26ddd7e04
997volt <p.halaburdzin@gmail.com>
2023-03-12 22:31:10 +0100

    add 2nd algorithm

--- Encrypter.java
+++ Encrypter.java
@@ -27,6 +27,20 @@ public class Encrypter {
         char encrypted = letter;
         if (unicodeAlg) {
             encrypted = (char)(letter + shift);
+        } else if (letter >= 'a' && letter <= 'z') {
+            encrypted = (char)(letter + shift);
+            if (encrypted > 'z') {
+                encrypted = (char)(encrypted - 26);
+            } else if (encrypted < 'a') {
+                encrypted = (char)(encrypted + 26);
+            }
+        } else if (letter >= 'A' && letter <= 'Z') {
+            encrypted = (char)(letter + shift);
+            if (encrypted > 'Z') {
+                encrypted = (char)(encrypted - 26);
+            } else if (encrypted < 'A') {
+                encrypted = (char)(encrypted + 26);
+            }
         }
         return encrypted;
     }
